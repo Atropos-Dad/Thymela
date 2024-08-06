c598fac495607e174f248361aab984ed0311e877
MiaBorkoo <23391707@studentmail.ul.ie>
2024-08-06 15:58:19 +0100

    Merge branch 'main' of https://github.com/Atropos-Dad/Thymela

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ from parsing.article_asseser import assess_article
 def main():
     # Example usage:
     article_url = "https://www.ebi.ac.uk/pride/archive/projects/PXD054577"
-    metadata_json = assess_article(article_url)
-    print("Extracted Metadata JSON:", metadata_json)
+    print(assess_article(article_url))
+    
 
     
 
