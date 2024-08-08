1da14672889ee0f61a4aa0e496d6afcac027d9ab
dylan-teehan-skc <dylan.teehan@gmail.com>
2024-08-08 15:01:22 +0100

    Remove unnecessary import

--- src/main.py
+++ src/main.py
@@ -2,13 +2,11 @@ import os
 import sys
 from tqdm import tqdm
 from dbwrap.db_get_study import get_n_studies
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from parsing.article_asseser import assess_article
 
 def main():
-    # Example usage:
-    studies = get_n_studies(10, "PRIDE")
 
+    studies = get_n_studies(10, "PRIDE")
     for study in tqdm(studies):
         article_url = ""
         output = assess_article(study) 
