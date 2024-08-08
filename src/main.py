4ba902235f2a5fb1f7b30b2a50f9e4618c9cd9ba
dylan-teehan-skc <dylan.teehan@gmail.com>
2024-08-08 15:07:53 +0100

    Clean Up main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,11 @@
 import os
 import sys
 from tqdm import tqdm
-from dbwrap.db_get_study import get_n_studies
 from parsing.article_asseser import assess_article
+from parsing.article_asseser import assess_multiple_studies
 
 def main():
-
-    studies = get_n_studies(10, "PRIDE")
-    for study in tqdm(studies):
-        article_url = ""
-        output = assess_article(study) 
+    print(assess_multiple_studies(10))
             
 if __name__ == "__main__":
     main()
