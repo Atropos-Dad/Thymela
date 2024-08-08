e608e13ad076ec1102f56d3c8bb63a6a8f427734
dylan-teehan-skc <dylan.teehan@gmail.com>
2024-08-08 14:36:49 +0100

    Modified it to read and input studies

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 import os
 import sys
-
+from tqdm import tqdm
+from dbwrap.db_get_study import get_n_studies
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from parsing.article_asseser import assess_article
 
 def main():
     # Example usage:
-    article_url = ""
-    print(assess_article(article_url, "tests/mock_data/Testing_File.txt"))
+    studies = get_n_studies(10, "PRIDE")
 
-    
-
+    for study in tqdm(studies):
+        article_url = ""
+        output = assess_article(study) 
+            
 if __name__ == "__main__":
     main()
