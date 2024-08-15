8c9d6359e15295c54a36d378690bfe004bec80f5
Jack Casey <jackcasey614@gmail.com>
2024-08-15 12:02:04 +0100

    modify app.py to use new search class

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, render_template, jsonify
 from dotenv import load_dotenv
 
 async def create_app():
-    from searching.search import search_for_articles # has to be here, need to initialize the chroma client/db before importing
+    from searching.search import ArticleSearcher # has to be here, need to initialize the chroma client/db before importing
+    searcher_instance = ArticleSearcher()
+    
     app = Flask(__name__)
     
     # Load environment variables
@@ -12,7 +14,7 @@ async def create_app():
     def index():
         if request.method == 'POST':
             query = request.form['query']
-            results = search_for_articles(query)
+            results = searcher_instance.search_for_articles(query)
             return render_template('results.html', results=results, query=query)
         return render_template('search_bar.html')
 
